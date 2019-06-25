359f488d0455c7ac929e91bf56443f02b0ea0e47
Michael Korir <mikor22@gmail.com>
2019-06-25 10:29:23 +0300

    create urls for the views

---
+++ insta/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'posts/$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_image, name='search_image'),
+    url(r'^profile/$',views.edit_profile_info, name='edit_profile_info'),
+    url(r'^upload/$',views.Photo, name='uploadPhoto'),
+    url(r'comment/(\d+)', views.comment, name='comment'),
+    url(r'^profile_info/$',views.profile, name='profile_info'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
